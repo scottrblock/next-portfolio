17281e09fa2263715d304f1429858c317a18e860
Scott Block <scottrblock@gmail.com>
2023-01-09 22:47:16 -0500

    add rel me to mastodon link

--- components/contact-card.jsx
+++ components/contact-card.jsx
@@ -1,4 +1,12 @@
-export function ContactCard({ icon, linkUrl, linkText }) {
+export function ContactCard({ icon, linkUrl, linkText, rel = undefined }) {
+  const linkAttrs = {
+    href: linkUrl
+  }
+
+  if (rel) {
+    linkAttrs.rel = rel
+  }
+
   return (
     <>
       <style global jsx>{`
@@ -15,7 +23,7 @@ export function ContactCard({ icon, linkUrl, linkText }) {
       <div className="contact-card">
         <div className="contact-card contact-card-icon">{icon}</div>
         <div className="details">
-          <a href={linkUrl}>{linkText}</a>
+          <a {...linkAttrs}>{linkText}</a>
         </div>
       </div>
     </>
