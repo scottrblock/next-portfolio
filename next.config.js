35e9deae1574af926735d892d074de6d4fea986a
Scott Block <scottrblock@gmail.com>
2025-10-12 21:44:06 -0400

    quick wip: out dir

--- next.config.js
+++ next.config.js
@@ -1,2 +1,8 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
-module.exports = withNextra()
+
+module.exports = withNextra({
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
+})
